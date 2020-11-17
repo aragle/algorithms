9945a3d2b8236c090609e065acbcf9e0329b1f5b
Ashiqur Rahman Alif <62181222+aratheunseen@users.noreply.github.com>
2020-11-17 13:01:36 +0600

    Create Grouping Children Problem Solution.cpp

---
+++ Greedy Algorithms/Grouping Children Problem Solution.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int getGroups(float children[]){
+
+    int lastChild = sizeof(children);
+    int currentChild=1;
+    int startingPoint=0;
+    int groups=0;
+
+    while(currentChild < lastChild){
+
+        int x = children[currentChild] - children[startingPoint];
+        if(x > 1){
+            startingPoint = currentChild ;
+            groups++;
+        }
+
+        if(currentChild==lastChild-1){
+            groups++;
+        }
+        currentChild++;
+    }
+    return groups;
+   }
+
+  int main() {
+
+    int n;
+    cout << "Number of Children: ";
+    cin >> n;
+
+    float children[n];
+
+    cout << "\nEnter Ages of " << n << " Children:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> children[i];
+    }
+    cout << endl << "Groups: " << getGroups(children) << endl << endl;
+  }
